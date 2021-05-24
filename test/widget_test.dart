281df01aed67b95629113a971550af82c9e57794
Leonardo <gomleo9@hotmail.com>
2021-05-24 09:11:39 -0300

    Architecture

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,17 +1,6 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility that Flutter provides. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
 import 'package:financial_app/features/login/presentation/pages/login.dart';
-import 'package:financial_app/features/register/presentation/pages/register.dart';
 import 'package:financial_app/shared/component_list.dart';
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:financial_app/main.dart';
 
 void main() {
   // Test structure
